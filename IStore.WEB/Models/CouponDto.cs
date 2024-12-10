9b45d319fe88afd565fbf34946e8562b36064903
Akhil Kukadiya <akhilkukadiya464@gmail.com>
2024-12-10 23:25:24 +0530

    coupon views added

--- IStore.WEB/Models/CouponDto.cs
+++ IStore.WEB/Models/CouponDto.cs
@@ -2,7 +2,7 @@
 {
     public class CouponDto
     {
-        public int CouponId { get; set; }
+        public int Id { get; set; }
         public string? CouponCode { get; set; }
         public double DiscountAmount { get; set; }
         public int MinAmount { get; set; }
