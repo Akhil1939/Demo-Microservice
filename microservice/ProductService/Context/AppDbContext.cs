2d908be9d0be087ce0b7b4b825f73a06ac634b3d
Akhil Kukadiya <akhilkukadiya464@gmail.com>
2025-09-01 09:20:56 +0530

    services

---
+++ microservice/ProductService/Context/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using ProductService.Models;
+
+namespace ProductService.Context;
+public class AppDbContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>().HasKey(p => p.Id);
+    }
+}
